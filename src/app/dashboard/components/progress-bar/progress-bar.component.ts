9be94dcb66002a53e354dc11673d58937cdd1a91
marius.boianciuc <marius.boianciuc@getik.net>
2021-03-03 19:11:03 +0200

    - added request for drinksConsumed

--- src/app/dashboard/components/progress-bar/progress-bar.component.ts
+++ src/app/dashboard/components/progress-bar/progress-bar.component.ts
@@ -8,9 +8,9 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: "app-progress-bar",
-  templateUrl: "./progress-bar.component.html",
-  styleUrls: ["./progress-bar.component.scss"],
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProgressBarComponent implements OnChanges {
